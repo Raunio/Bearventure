748798208e1e915243fbb910ac4de0002a2ff09e
Niko Tiikkaja <niko.tiikkaja@saunalahti.fi>
2013-10-31 19:01:11 +0200

    Added missing files

---
+++ Bearventure/Bearventure/Gameplay/GameObjects/Platform.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Bearventure.Gameplay.Characters;
+using Microsoft.Xna.Framework.Content;
+
+namespace Bearventure.Gameplay.GameObjects
+{
+    public class Platform : GameplayObject
+    {
+        Constants.PlatformType type;
+
+        public Platform(ContentManager content, Constants.PlatformType type, Vector2 position)
+        {
+            this.type = type;
+            this.position = position;
+
+            Initialize(content);
+        }
+
+        private void Initialize(ContentManager content)
+        {
+            switch (type)
+            {
+                case Constants.PlatformType.Basic:
+                    currentAnimation = new Animation(content.Load<Texture2D>(Constants.BasicPlatform), 0, 80, 25, 0, 1, 25);
+                    break;
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            currentAnimation.Animate(gameTime);
+        }
+    }
+}
