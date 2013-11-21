10a95e62ceb705b3a4a821ecc5af72cc5691f8db
Niko Tiikkaja <niko.tiikkaja@saunalahti.fi>
2013-11-21 17:09:41 +0200

    Added missing files

---
+++ Bearventure/Bearventure/Engine/CollisionDetection/ObjectCollisionEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bearventure.Gameplay;
+
+namespace Bearventure.Engine.CollisionDetection
+{
+    public class ObjectCollisionEvent
+    {
+        private GameplayObject a, b;
+        private int collision_location;
+
+        /// <summary>
+        /// Returns the bounding box location of the collision.
+        /// </summary>
+        public int CollisionLocation
+        {
+            get
+            {
+                return collision_location;
+            }
+        }
+
+        /// <summary>
+        /// The object that starts the collision event.
+        /// </summary>
+        public GameplayObject A
+        {
+            get
+            {
+                return a;
+            }
+        }
+
+        /// <summary>
+        /// The object that A collides into.
+        /// </summary>
+        public GameplayObject B
+        {
+            get
+            {
+                return b;
+            }
+        }
+
+        public ObjectCollisionEvent(GameplayObject a, GameplayObject b, int collision_location)
+        {
+            this.a = a;
+            this.b = b;
+            this.collision_location = collision_location;
+        }
+
+        public void ApplyPushForce()
+        {
+
+        }
+    }
+}
